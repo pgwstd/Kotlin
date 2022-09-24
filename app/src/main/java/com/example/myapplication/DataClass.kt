2c6a14fee3b44c6c66e4f1803e1a6eee0780164c
pgwstr <2712278005@qq.com>
2022-09-24 20:57:48 +0800

    练习

--- app/src/main/java/com/example/myapplication/DataClass.kt
+++ app/src/main/java/com/example/myapplication/DataClass.kt
@@ -1,5 +1,5 @@
 package com.example.myapplication
-data class Cellphone(val brand: String, val price: Float)  //声明一个有参构造方法
+data class Cellphone(val brand: String, val price: Float)  //声明一个有参构造方法(data修饰证明它是一个数据类)
 fun main(){
     val cellphone = Cellphone("Xiaomi", 2399f)
     val cellphone2 = Cellphone("Xiao", 3399f)

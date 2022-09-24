31655f89c77ad7fad70ef5c78d161afc5311399b
pgwstr <2712278005@qq.com>
2022-09-24 19:02:10 +0800

    练习

--- app/src/main/java/com/example/myapplication/BranChing.kt
+++ app/src/main/java/com/example/myapplication/BranChing.kt
@@ -1,21 +1,34 @@
 package com.example.myapplication
 
-fun main(){
-    //if分支学习
-    val age: Int = 15
-    if (age < 18)
-        println("末成年")
-    else
-        println("成年了")
+//fun main(){
+//    //if分支学习
+//    val age: Int = 15
+//    if (age < 18)
+//        println("末成年")
+//    else
+//        println("成年了")
+//
+//
+//    println("*************")
+//    val marriage = true  //定义boolean类型的变量
+//    if (marriage){
+//        println("为真")
+//    }
+//    else {
+//        println("为假")
+//    }
+//    println("******************")
+//    val gender: Any = 0
+//    if (gender == 0){
+//        println(0)
+//    }
+//    else if (gender == 1){
+//        println(1)
+//    }
+//    else{
+//        println(-1)
+//
+//    }
+//
+//}
 
-
-    println("*************")
-    val marriage = true  //定义boolean类型的变量
-    if (marriage){
-        println("为真")
-    }
-    else {
-        println("为假")
-    }
-
-}

90a5b28a2277ae593eb96ad8adfa1377b4a03952
pgwstr <2712278005@qq.com>
2022-09-24 19:17:55 +0800

    练习

--- app/src/main/java/com/example/myapplication/BranChing.kt
+++ app/src/main/java/com/example/myapplication/BranChing.kt
@@ -33,14 +33,42 @@ package com.example.myapplication
 //}
 
 
+//fun main() {
+//    //when分支，像switch
+//    var input: Any = 0
+//    when (input) {
+////        0 -> println("0")
+////        1 -> println("1")
+////        2 -> println("2")
+//        in 0..2 -> println("012") //从0..2这个区间都可以被输去（左闭右闭）
+//        else -> println(-1)
+//    }
+//
+//    when (input) {
+//        is Int -> println("是一个整数类型的")
+//        else -> println("不是整数类型的")
+//    }
+//}
+
 fun main(){
-    //when分支，像switch
-    var input: Any = 0
-    when(input){
-//        0 -> println("0")
-//        1 -> println("1")
-//        2 -> println("2")
-        in 0 .. 2 -> println("012") //从0..2这个区间都可以被输去（左闭右闭）
-        else -> println(-1)
+
+    val edu: Int = 2
+    val subject: Int = 2
+    when(edu){
+        1 -> println("专科")
+        2 -> println("本科")
+        3 -> println("研究生")
+        4 -> println("博士")
+        else -> println("专升本")
+
     }
+
+    when(subject){
+        1, 2 -> println("文学学士")  //两个选择
+        in 3 .. 5 -> println("理学学士")  //区间选择
+        6 -> println("工学学士")
+        7 -> println("农学学士")
+        else -> println("基他学士")
+    }
+
 }

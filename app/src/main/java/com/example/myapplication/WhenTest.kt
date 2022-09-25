54ff5836fa76b85862745d03f611c741003279bc
pgwstr <2712278005@qq.com>
2022-09-25 21:57:06 +0800

    练习

--- app/src/main/java/com/example/myapplication/WhenTest.kt
+++ app/src/main/java/com/example/myapplication/WhenTest.kt
@@ -1,12 +1,34 @@
 package com.example.myapplication
 
-fun getSorce(name: String) = when {
-    name.startsWith("Tom") -> 86
-    name == "Jim" -> 77
-    name == "Jack" -> 98
-    name == "Lily" -> 100
-    else -> 0
-}
+//方式一
+//fun getSorce(name: String) = when {
+//    name.startsWith("Tom") -> 86  //startsWith检测是否以指定字符开始
+//    name == "Jim" -> 77
+//    name == "Jack" -> 98
+//    name == "Lily" -> 100
+//    else -> 0
+//}
+
+//方式二
+//fun getSorce(name: String) = when (name) {
+//    "Tom" -> 86  //startsWith检测是否以指定字符开始
+//    "Jim" -> 77
+//    "Jack" -> 98
+//    "Lily" -> 100
+//    else -> 0
+//}
+
+
+//方式三
+fun getSorce(name: String) =
+    if (name == "Tom") {
+        86
+    } else if (name == "Jim") {
+        77
+    } else {
+        0
+    }
+
 
 fun main() {
     println(getSorce("Tom"))

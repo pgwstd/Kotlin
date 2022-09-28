c9343d908e3926b5c7c625b3ff5b75d26b390b6e
pgwstr <2712278005@qq.com>
2022-09-28 23:22:58 +0800

    练习

---
+++ app/src/main/java/com/example/myapplication/HomeWork.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+interface Run{
+    fun go(){
+        println("跑了")
+    }
+}
+
+open class RacingCar(val position: Int)
+open class Mercedes
+open class Ferrari
+open class BMW
+
+
+
+fun main(){
+
+
+
+}

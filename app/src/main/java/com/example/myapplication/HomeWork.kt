e8086bb0c7fde8b35b8db578ea12db7a61d9b6a7
pgwstr <2712278005@qq.com>
2022-09-30 15:52:06 +0800

    练习

--- app/src/main/java/com/example/myapplication/HomeWork.kt
+++ app/src/main/java/com/example/myapplication/HomeWork.kt
@@ -1,20 +1,49 @@
-package com.example.myapplication
+package com.example.helloworld
 
+import java.lang.Math.random
+
+//Run接口与跑的方法
 interface Run{
-    fun go(){
-        println("跑了")
+    fun go(name: String,position: Int){
+        println("车开始跑")
     }
 }
 
 open class RacingCar(val position: Int)
-open class Mercedes
-open class Ferrari
-open class BMW
+
+
+
+class Mercedes(position: Int, name: String): RacingCar(position), Run {
+    override fun go(name: String, position: Int) {
+        println("$name:$position")
+
+    }
+
+}
+class Ferrari(position: Int, name: String):RacingCar(position), Run {
+    override fun go(name: String,position: Int) {
+        println("$name:$position")
+
+    }
+}
+
+class BMW(position: Int, name: String):RacingCar(position), Run {
+    override fun go(name: String,position: Int) {
+        println("$name:$position")
+
+    }
+}
 
 
 
 fun main(){
-
-
-
+    val M = Mercedes(75,"Mercedes")
+    val F = Ferrari(23,"Ferrari")
+    val B = BMW(10,"BMW")
+    while (true) {
+        val randoms = (1..5).random()
+        M.go("Mercedes", randoms+1)
+        F.go("Ferrari", randoms+1)
+        B.go("BMW", randoms+1)
+    }
 }

64c3eb191c7ab456d93480a1c089f2ad7c2dfd99
pgwstr <2712278005@qq.com>
2022-10-01 19:43:21 +0800

    练习

--- app/src/main/java/com/example/myapplication/HomeWork.kt
+++ app/src/main/java/com/example/myapplication/HomeWork.kt
@@ -1,23 +1,83 @@
 package com.example.myapplication
-
+//方式一
 
 //Run接口与跑的方法
+//interface Run{
+//    fun go(position: Int,name: String){
+//        println("车开始跑")
+//    }
+//}
+//
+//open class RacingCar(val position: Int)
+//
+//
+//class Mercedes(position: Int, name: String): RacingCar(position), Run {
+//    override fun go(position: Int,name: String) {
+//        println("$name:$position")
+//    }
+//
+//
+//}
+//class Ferrari(position: Int, name: String):RacingCar(position), Run {
+//    override fun go(position: Int,name: String) {
+//        println("$name:$position")
+//    }
+//}
+//
+//class BMW(position: Int, name: String):RacingCar(position), Run {
+//    override fun go(position: Int,name: String) {
+//        println("$name:$position")
+//    }
+//}
+//
+//
+//fun main(){
+//    var Mspend = 1
+//    var Fspend = 1
+//    var Bspend = 1
+//    var SW = true
+//    val M = Mercedes(75,"Mercedes")
+//    val F = Ferrari(23,"Ferrari")
+//    val B = BMW(10,"BMW")
+//    while (SW) {
+//        Mspend += ((1..5).random())
+//        Fspend += ((1..5).random())
+//        Bspend += ((1..5).random())
+//        M.go(Mspend,"Mercedes")
+//        F.go(Fspend,"Ferrari")
+//        B.go(Bspend,"BMW")
+//        println("----------------------")
+//        if (Mspend >= 200){
+//            println("The Winner is: Mercedes")
+//            SW = false
+//        }
+//        else  if (Fspend >= 200){
+//            println("The Winner is: Ferrari")
+//            SW = false
+//        }
+//        else if (Bspend >= 200){
+//            println("The Winner is: BMW")
+//            SW = false
+//        }
+//    }
+//}
+
+//方式二
 interface Run{
     fun go(position: Int,name: String){
         println("车开始跑")
     }
 }
 
-open class RacingCar(val position: Int)
+open class RacingCar(var position: Int)
 
 
 class Mercedes(position: Int, name: String): RacingCar(position), Run {
     override fun go(position: Int,name: String) {
         println("$name:$position")
     }
-
-
 }
+
 class Ferrari(position: Int, name: String):RacingCar(position), Run {
     override fun go(position: Int,name: String) {
         println("$name:$position")
@@ -32,32 +92,34 @@ class BMW(position: Int, name: String):RacingCar(position), Run {
 
 
 fun main(){
-    var Mspend = 1
-    var Fspend = 1
-    var Bspend = 1
-    var SW = true
-    val M = Mercedes(75,"Mercedes")
-    val F = Ferrari(23,"Ferrari")
-    val B = BMW(10,"BMW")
-    while (SW) {
-        Mspend += ((1..5).random())
-        Fspend += ((1..5).random())
-        Bspend += ((1..5).random())
-        M.go(Mspend,"Mercedes")
-        F.go(Fspend,"Ferrari")
-        B.go(Bspend,"BMW")
-        println("----------------------")
-        if (Mspend >= 200){
+    var mSpend = 1
+    var fSpend = 1
+    var bSpend = 1
+    val mercedes = Mercedes(75,"Mercedes")
+    val ferrari = Ferrari(23,"Ferrari")
+    val bmw = BMW(10,"BMW")
+    while (true) {
+        mSpend += ((1..5).random())
+        fSpend += ((1..5).random())
+        bSpend += ((1..5).random())
+        mercedes.go(mSpend,"Mercedes")
+        if (mSpend >= 200){
+            println("----------------------")
             println("The Winner is: Mercedes")
-            SW = false
+            break
         }
-        else  if (Fspend >= 200){
+        ferrari.go(fSpend,"Ferrari")
+        if (fSpend >= 200){
+            println("----------------------")
             println("The Winner is: Ferrari")
-            SW = false
+            break
         }
-        else if (Bspend >= 200){
+        bmw.go(bSpend,"BMW")
+        if (bSpend >= 200){
+            println("----------------------")
             println("The Winner is: BMW")
-            SW = false
+            break
         }
+        println("----------------------")
     }
 }

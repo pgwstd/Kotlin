b0ba1fd6c900e7438f84b64c0f5d690c2bee2a95
pgwstr <2712278005@qq.com>
2022-10-01 19:46:39 +0800

    练习

--- app/src/main/java/com/example/myapplication/HomeWork.kt
+++ app/src/main/java/com/example/myapplication/HomeWork.kt
@@ -47,6 +47,7 @@ package com.example.myapplication
 //        F.go(Fspend,"Ferrari")
 //        B.go(Bspend,"BMW")
 //        println("----------------------")
+//        有问题，会出现两个200
 //        if (Mspend >= 200){
 //            println("The Winner is: Mercedes")
 //            SW = false

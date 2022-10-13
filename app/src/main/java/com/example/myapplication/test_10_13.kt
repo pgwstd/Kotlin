19d7c937e3d42d84d7cbadb51250a3ac10e7bdce
pgwstr <2712278005@qq.com>
2022-10-13 23:48:41 +0800

    练习

---
+++ app/src/main/java/com/example/myapplication/test_10_13.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+/**
+ * @author pgwstr
+ * @date 2022/10/13 23:37
+ */
+
+fun main()
+{
+
+    //TODO  var 可读可改
+    var name : String = "xiaoming"
+    name = "xiaxia"
+    println(name)
+
+    //TODO val 只读
+    val age : Int = 99
+    println(age)
+
+    val oo = 1
+    println(oo)
+}

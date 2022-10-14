9e59744fc0adcd56adc9ab3efb4493d2062ec992
pgwstr <2712278005@qq.com>
2022-10-14 22:25:53 +0800

    练习

--- app/src/main/java/com/example/myapplication/test_10_13.kt
+++ app/src/main/java/com/example/myapplication/test_10_13.kt
@@ -4,19 +4,18 @@ package com.example.myapplication
  * @author pgwstr
  * @date 2022/10/13 23:37
  */
+    val Pi = 3.14
 
 fun main()
 {
-
     //TODO  var 可读可改
     var name : String = "xiaoming"
     name = "xiaxia"
     println(name)
-
     //TODO val 只读
     val age : Int = 99
     println(age)
-
     val oo = 1
     println(oo)
+
 }

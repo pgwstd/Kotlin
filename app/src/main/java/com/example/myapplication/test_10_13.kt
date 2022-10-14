72b588ec332bc5efa678c32e6ae20da1aaee3d06
pgwstr <2712278005@qq.com>
2022-10-14 23:00:45 +0800

    练习

--- app/src/main/java/com/example/myapplication/test_10_13.kt
+++ app/src/main/java/com/example/myapplication/test_10_13.kt
@@ -18,4 +18,10 @@ fun main()
     val oo = 1
     println(oo)
 
+    val nm = "公园"
+    val time = 6
+    println("小明在${nm}玩了$time")
+    
+    val isOK = true
+    println("${if (isOK) "登录成功" else "登录失败"}")
 }

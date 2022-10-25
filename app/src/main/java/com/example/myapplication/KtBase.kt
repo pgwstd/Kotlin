b5d08d09a194ac375556d465f364fa5d36f159ae
pgwstr <2712278005@qq.com>
2022-10-25 22:35:34 +0800

    练习

---
+++ app/src/main/java/com/example/myapplication/KtBase.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+/**
+ * @author pgwstr
+ * @date 2022/10/25 22:20
+ * 匿名函数的使用
+ */
+fun main() {
+    //函数输入输出声明
+    val methodAction: () -> String
+
+    //实现函数
+    methodAction = {
+        val inputValue = 999
+        //匿名函数最后一行就是返回值
+        "$inputValue pgwstr"
+
+    }
+    //调用输出函数
+    println(methodAction)
+}

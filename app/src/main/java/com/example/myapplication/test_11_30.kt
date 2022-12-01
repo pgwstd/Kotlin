f4bcf49ebf43b477e2807130a2461bc46a0f95ca
panguangwei <2712278005@qq.com>
2022-12-01 10:29:52 +0800

    学习12-1

--- app/src/main/java/com/example/myapplication/test_11_30.kt
+++ app/src/main/java/com/example/myapplication/test_11_30.kt
@@ -6,9 +6,12 @@ package com.example.myapplication
         val bignum = 800000000  //如果超过int的最大值，就是long类型
         val longnum = 20L   //数字后面显式加l，表明是long类型的
         val bytenum: Byte = 1 //显式声明这是byte类型的
-
+        val dounum = 3.1415926535   //默认小数格式是double
+        val flanum = 3.1415926535f  //由于float的位宽问题，只保留6位（十进制是6位，后面的小数会四舍五入）
         println(num)
         println(bignum)
         println(longnum)
         println(bytenum)
+        println(dounum)
+        println(flanum)
     }
